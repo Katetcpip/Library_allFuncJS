3c6cc704103e0537ccf17cc3b01e1aa6ea9ffcc3
Katetcpip <90146349+Katetcpip@users.noreply.github.com>
2022-09-18 13:08:06 +0300

    you can change ur book now

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
-const books = [{
+const addModal = document.getElementById('add-modal')
+const closeModalButton = document.getElementById('close-modal-button')
+const openModalButton = document.getElementById('open-modal-button')
+
+
+function closeModal() {
+    addModal.style.display = 'none'
+}
+
+
+function openModal() {
+    addModal.style.display = 'flex'
+}
+
+closeModalButton.addEventListener('click', closeModal)
+openModalButton.addEventListener('click', openModal)
+
+
+
+const changeModal = document.getElementById('change-modal')
+const changeButton = document.getElementById('buttonChange')
+const closeChangeModalButton = document.getElementById('close-change-button')
+const acceptChangeModalButton = document.getElementById('accept-change-button')
+
+
+function closeChangeModal() {
+    changeModal.style.display = 'none'
+}
+closeChangeModalButton.addEventListener('click', closeChangeModal)
+
+
+function buttonChange(id) {
+    const book = books.find((s) => {
+        return s.id === id
+    })
+
+    const bookIndex = books.indexOf(book)
+
+    document.getElementById("title2").value = books[bookIndex].title;
+    document.getElementById("authors2").value = books[bookIndex].authors;
+    document.getElementById("year2").value = books[bookIndex].year;
+    document.getElementById("link2").value = books[bookIndex].image;
+
+    changeModal.style.display = 'flex'
+}
+
+
+let GodObj = {};
+
+function indexChange(id) {
+    const book = books.find((s) => {
+        return s.id === id
+    })
+
+    const bookIndex = books.indexOf(book)
+    GodObj.id = bookIndex;
+    console.log(bookIndex)
+    console.log(GodObj.id)
+}
+
+
+let books = [{
         id: 1,
         title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
         authors: 'Erich Gamma, John Vlissides, Ralph Johnson, Richard Helm',
@@ -28,7 +89,6 @@ const books = [{
     }
 ]
 
-
 const container = document.getElementById("container")
 
 function renewBooks() {
@@ -36,40 +96,58 @@ function renewBooks() {
     books.forEach((book) => {
         container.innerHTML +=
             `
-        <div class="book-cont">
-        
+        <div class="book-cont" id='book-cont'>
+
             <img class="image" src="${book.image}">
             <p class="title">${book.title}</p>
             <p class="year">${book.year}</p>
-           
+
           <div class="authors">
-            <p class="author">${book.authors}</p>
+            <p class="author main">${book.authors}</p>
            </div>
             <div class="buttons">
-            <button onclick="buttonChange()" class="button add">Изменить</button>
+            <button onclick="indexChange(${book.id}); buttonChange(${book.id})" class="button add">Изменить</button>
             <button onclick="buttonDelete(${book.id})" class="button">Удалить</button>
             </div>
-            
+
         </div>
         `
     })
 }
 
 
-
-let isOpen = false;
-const jsform = document.getElementById("hidden")
-
-
-function openForm() {
-    if (isOpen) {
-        jsform.style.display = "none";
-        isOpen = false;
-    } else {
-        jsform.style.display = "flex";
-        isOpen = true;
-    }
+function workJson() {
+    const booksJson = JSON.stringify(books)
+    localStorage.setItem('books', booksJson)
 }
+
+
+
+function changingBook() {
+
+    let bookIndex2 = GodObj.id;
+    console.log(GodObj.id)
+    console.log(bookIndex2)
+
+
+    const titleValue = document.getElementById("title2").value
+    const authorsValue = document.getElementById("authors2").value
+    const yearValue = document.getElementById("year2").value
+    const linkValue = document.getElementById("link2").value
+
+    const book = {
+        title: titleValue,
+        authors: authorsValue,
+        year: yearValue,
+        image: linkValue
+    }
+
+    books.splice(bookIndex2, 1, book)
+    renewBooks()
+    closeChangeModal()
+    workJson()
+}
+
 
 function addBook() {
 
@@ -84,6 +162,7 @@ function addBook() {
         year: yearValue,
         image: linkValue
     }
+
     books.push(book)
     renewBooks()
 
@@ -92,8 +171,10 @@ function addBook() {
     document.getElementById("year").value = ""
     document.getElementById("link").value = ""
 
-    jsform.style.display = "none";
+    closeModal()
+    workJson()
 }
+
 
 
 
@@ -102,10 +183,25 @@ function buttonDelete(id) {
         return s.id === id
     })
 
+
+
     const bookIndex = books.indexOf(book)
     books.splice(bookIndex, 1)
 
-    renewBooks();
+    renewBooks()
+    workJson()
+}
+
+const myButton = document.getElementById('add-student-button')
+myButton.addEventListener('click', addBook)
+
+const newButton = document.getElementById('newBook-button')
+newButton.addEventListener('click', changingBook)
+
+
+const booksJson = localStorage.getItem('books')
+if (booksJson) {
+    books = JSON.parse(booksJson)
 }
 
 renewBooks()
